8d171230ae8d990351d25f74598cff0854d7c8b8
Piotr Zuber <piotr_zuber@interia.pl>
2017-04-26 23:48:17 +0200

    Stworzenie klasy Package, dodanie konkrtuktora

--- Package master/Package1.cs
+++ Package master/Package1.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace Package_master
 {
-    class Package1
+    class Package
     {
+        private double Height;//wysokość
+        private double Weight;//szerokość
+
+        //konstruktor
+        public Package(double Height, double Weight)
+        {
+            if (Height < 0.1 || Weight < 0.1)
+            {
+                throw new Exception("Podano zbyt małe rozmary paczki");
+            }
+            else
+            {
+                this.Height = Height;
+                this.Weight = Weight;
+            }
+        }
     }
 }
